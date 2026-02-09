696598e3d94931a7ddacb62ee153197b56f6cfad
Эдуард Чмыхов <>
2026-02-09 19:46:32 +0300

    httpServer_done

--- internal/app/httpserver/clinics/create_patient.go
+++ internal/app/httpserver/clinics/create_patient.go
@@ -9,7 +9,6 @@ import (
 
 func (r *httpRouter) CreatePatient(w http.ResponseWriter, req *http.Request) {
 	bodyBytes, readErr := io.ReadAll(req.Body)
-	r.logger.Info("CreatePatient", bodyBytes)
 	if readErr != nil {
 		r.logger.ErrorContext(req.Context(), "failed to read request body: %w", readErr)
 		http.Error(w, "BAD_REQUEST", http.StatusBadRequest)
@@ -31,22 +30,22 @@ func (r *httpRouter) CreatePatient(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	response, err := r.usecase.CreatePatient(req.Context(), requestCreatePatient)
 	if err != nil {
-		r.logger.ErrorContext(req.Context(), "failed to create patient: %w", err)
+		r.logger.ErrorContext(req.Context(), "failed to create patient:", err)
 		w.WriteHeader(http.StatusBadRequest)
-		if encodeErr := json.NewEncoder(w).Encode(err.Error());
-		//if encodeErr := json.NewEncoder(w).Encode(models.CreatePatientResponse{
-		//Status: "FAILED",	}
-		encodeErr != nil {
-			r.logger.ErrorContext(req.Context(), "failed to encode response: %w", encodeErr)
+		if encodeErr := json.NewEncoder(w).Encode(models.ErrorResponse{
+			Error: err.Error(),
+		}); encodeErr != nil {
+			r.logger.ErrorContext(req.Context(), "failed to encode response:", encodeErr)
 		}
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 	patientId := r.mapper.CreatePatientToHttp(response)
+	r.logger.Info("patient created: First Name:", request.FirstName, "Last Name:", request.LastName)
 	if encodeErr := json.NewEncoder(w).Encode(models.CreatePatientResponse{
 		Status:    "SUCCESS",
 		PatientId: patientId.PatientId,
 	}); encodeErr != nil {
-		r.logger.ErrorContext(req.Context(), "failed to encode response: %w", encodeErr)
+		r.logger.ErrorContext(req.Context(), "failed to encode response:", encodeErr)
 	}
 }
